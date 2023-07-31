4b5b05a2abe87ada73580128500bdb2466fec777
ganeshrajugalla <ganesh.galla@techaheadcorp.com>
2023-08-01 01:11:51 +0530

    ReactiveTableView

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ReactiveTableView' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ReactiveTableView
+
+pod "RxSwift"
+pod "RxCocoa"
+
+end
--- ReactiveTableView/ViewController.swift
+++ ReactiveTableView/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
+    
+    // ------------------------------------------------------------------------------
+    // MARK: DECLARED OUTLETS
+    // ------------------------------------------------------------------------------
+    @IBOutlet weak var tblView:UITableView!
+    
+    // ------------------------------------------------------------------------------
+    // MARK: DECLARED VARIABLES
+    // ------------------------------------------------------------------------------
+    let disposeBag = DisposeBag()
+    let tableViewRxItems = Observable.just(["Item:- 1", "Item:- 2","Item:- 3","Item:- 4"])
+    
 
+    // ------------------------------------------------------------------------------
+    // MARK: SCREEN SETUP
+    // ------------------------------------------------------------------------------
+        
+    // (View Did Load)
+    // --------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setTableViewRx()
     }
-
-
+    
+    // FUNCTION: Setup TableView
+    // ----------------------------
+    private func setTableViewRx(){
+        tableViewRxItems
+            .bind(to: tblView
+            .rx
+            .items(cellIdentifier: "tblCell")){
+                (tv,tableItems,cell) in
+                cell.textLabel?.text = tableItems
+            }.disposed(by: disposeBag)
+    }
 }
 
